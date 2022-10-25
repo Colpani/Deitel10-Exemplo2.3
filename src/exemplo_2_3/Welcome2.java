83026d5ad2bcd4dcdb67272ff835a94aef4f8a14
Marcel Colpani <marceluiz@gmail.com>
2022-10-25 15:11:54 -0300

    Exemplo 2.3

---
+++ src/exemplo_2_3/Welcome2.java
@@ -0,0 +1,10 @@
+package exemplo_2_3;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}
+
+}
